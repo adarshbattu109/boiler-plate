6603e14dc863af628d27fa2cefda9bfe82905fba
Adarsh Battu <adarsh.battu109@gmail.com>
2022-10-13 13:16:23 +0530

    Added calls to clear screen on program start

--- src/boilerplate/__init__.py
+++ src/boilerplate/__init__.py
@@ -1,11 +1,14 @@
 """Start point for `boiler-plate`"""
 
+import os
 from boilerplate.boilerplate import gather_requirements, execute_steps
 from boilerplate.messages import MISSON_ABORT, WELCOME_MSG
 
 
 def main() -> None:
     """Main Method to be called upon to create the `Project` as per user"""
+
+    _ = os.system("cls") if os.name == "nt" else os.system("clear")
     print(WELCOME_MSG)
     user_inputs = gather_requirements()
 
